79e2c0779281cecd59bdf8580dc82158d81a9e3d
cemacar <cem.acar@smail.th-koeln.de>
2024-04-25 14:54:48 +0200

    Initial commit

---
+++ cmake-build-root-docker/CMakeFiles/Praktikum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Praktikum.dir/main.c.o"
+  "Praktikum.pdb"
+  "Praktikum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Praktikum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
